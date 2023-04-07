ad20e2157beddaa1e4ce124c511885cc9c3263ac
Emill0004 <e.s.miller13125@gmail.com>
2023-04-07 02:44:30 -0600

    rewrote code to be more intuitive, and added a clear history button.

--- script.js
+++ script.js
@@ -1,69 +1,108 @@
-var stockEl = document.getElementById("stockL")
-var searchBtnEl = document.getElementById("searchbtn");
-var txtBoxEl = document.getElementById("usrinpt");
-var stockHistEl = document.getElementById("stockHistL")
+var stockListEl = document.getElementById("stockL")
+var searchBtn = document.getElementById("searchBtn");
+var clearBtn = document.getElementById("clearBtn");
+var inputEl = document.getElementById("usrinpt");
+var historyList = document.getElementById("stockHistL");
 var requestURL = 'https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2023-03-31?adjusted=true&apiKey=1cw4p56EVUjieQaTpTQxhiapI8vsGjCF';
 var stockList;
 var result;
 var stocks = [];
 var defaultStocks = ["NOW", "GOOG", "AMZN", "DIS", "PANW", "BA", "PLD", "JNJ"];
+var historyArray = [];
 
 
-if('geolocation' in navigator){
-    navigator.geolocation.getCurrentPosition(setPosition);
- }
- function setPosition(position){
-     let latitude = position.coords.coords.latitude;
-     let longitude = position.coords.longitude;
-     getWeather(latitude, longitude);
- }
+// Leah's location Code \\
+// if('geolocation' in navigator){
+//     navigator.geolocation.getCurrentPosition(setPosition);
+//  }
+//  function setPosition(position){
+//      let latitude = position.coords.coords.latitude;
+//      let longitude = position.coords.longitude;
+//      getWeather(latitude, longitude);
+//  }
 
-function getApi(url, search) {
+function getApi(url) {
     fetch(url)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            // console.log(data);
             stockList = data.results;
-            filterApi(stockList, search);
+            localStorage.setItem("Stock List", JSON.stringify(stockList));
         })
 };
 
-function filterApi(stockList, search) {
-    result = stockList.find(s => s.T === search);
-    stocks.push(result.T + ", close: " + result.c + ", high: " + result.h + ", low: " + result.l + ", transactions: " + result.n);
-    renderStocks(result)
-};
+getApi(requestURL);
 
-function renderStocks(result) {
-    stockEl.innerHTML = "";
+var testStockList = JSON.parse(localStorage.getItem("Stock List"));
+console.log(testStockList);
+var testStock = testStockList.find(s => s.T === "AMZN");
+console.log(testStock);
+
+function renderStocks() {
+    stockListEl.innerHTML = "";
     for (var i = 0; i < stocks.length; i++) {
         var stock = stocks[i];
-        var li = document.createElement("li");
-        li.textContent = stock;
-        stockEl.appendChild(li);
-    }
-}
+        var stockLi = document.createElement("li");
+        stockLi.textContent = stock;
+        stockListEl.appendChild(stockLi);
+    }};
 
-// getApi(requestURL, "GOOG");
+function newInit() {
+    for (var i = 0; i < defaultStocks.length; i++) {
+        result = testStockList.find(s => s.T === defaultStocks[i]);
+        stocks.push(result.T + ", close: " + result.c + ", high: " + result.h + ", low: " + result.l + ", transactions: " + result.n);
+        renderStocks();
+        renderHistory();
+    }};
 
+newInit();
 
-searchBtnEl.addEventListener("click", function() {
-    getApi(requestURL, txtBoxEl.value);
-    txtBoxEl.value = "";
+function newFilterApi(search) {
+    result = testStockList.find(s => s.T === search);
+    stocks.push(result.T + ", close: " + result.c + ", high: " + result.h + ", low: " + result.l + ", transactions: " + result.n);
+    renderStocks();
+};
+
+searchBtn.addEventListener("click", function() {
+    var input = inputEl.value.toUpperCase();
+    newFilterApi(input);
+    historyArray.push(input);
+    inputEl.value = "";
+    localStorage.setItem("Search History", JSON.stringify(historyArray));
+    renderHistory();
+});
+
+clearBtn.addEventListener("click", function() {
+    historyArray = [];
+    localStorage.setItem("Search History", JSON.stringify(historyArray));
+    renderHistory();
 })
 
-function init(stockList) {
-    for (var i = 0; i < defaultStocks.length; i++) {
-        result = stockList.find(s => s.T === defaultStocks[i]);
-        stocks.push(result.T + ", close: " + result.c + ", high: " + result.h + ", low: " + result.l + ", transactions: " + result.n);
-        renderStocks(result); 
+function getHistory() {
+    var storedHistory = JSON.parse(localStorage.getItem("Search History"));
+    if(storedHistory !== null) {
+        historyArray = storedHistory;
     }
-    
-}:
+};
+
+function renderHistory() {
+    getHistory();
+    historyList.innerHTML = "";
+    for (var i = 0; i < historyArray.length; i++){
+        var histLi = document.createElement("li");
+        histLi.textContent = historyArray[i];
+        historyList.appendChild(histLi);
+    }
+};
 
 // TODO":
 // most recent buisness day
-// populate initial load with default stocks
-// 
+// Make the searches clickable
+
+// Process:
+// fetch the api and set an array equal to the results.
+// add a function to populate default stocks when the page is loaded
+// add a search bar and button to search a specific stock by its ticker symbol
+// add a history array that interacts with local storage
+// make the history lis clickable to re-search them
