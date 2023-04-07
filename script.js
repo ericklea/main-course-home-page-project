f30677004f2f93ff07180d72da94cc76dbd84a16
Emill0004 <e.s.miller13125@gmail.com>
2023-04-06 19:21:41 -0600

    added user search functionality for stocks.

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-var inputEl = document.getElementById("userInput");
-var searchBttnEl = document.getElementById("searchBttn")
+var stockEl = document.getElementById("stockL")
+var searchBtnEl = document.getElementById("searchbtn");
+var txtBoxEl = document.getElementById("usrinpt");
+var stockHistEl = document.getElementById("stockHistL")
 var requestURL = 'https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2023-03-31?adjusted=true&apiKey=1cw4p56EVUjieQaTpTQxhiapI8vsGjCF';
 var stockList;
 var result;
+var stocks = [];
+var defaultStocks = ["NOW", "GOOG", "AMZN", "DIS", "PANW", "BA", "PLD", "JNJ"];
+
+
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition);
+ }
+ function setPosition(position){
+     let latitude = position.coords.coords.latitude;
+     let longitude = position.coords.longitude;
+     getWeather(latitude, longitude);
+ }
 
 function getApi(url, search) {
     fetch(url)
@@ -10,25 +24,46 @@ function getApi(url, search) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
+            // console.log(data);
             stockList = data.results;
             filterApi(stockList, search);
         })
 };
 
 function filterApi(stockList, search) {
-    console.log(stockList);
     result = stockList.find(s => s.T === search);
-    console.log(result)
+    stocks.push(result.T + ", close: " + result.c + ", high: " + result.h + ", low: " + result.l + ", transactions: " + result.n);
+    renderStocks(result)
 };
+
+function renderStocks(result) {
+    stockEl.innerHTML = "";
+    for (var i = 0; i < stocks.length; i++) {
+        var stock = stocks[i];
+        var li = document.createElement("li");
+        li.textContent = stock;
+        stockEl.appendChild(li);
+    }
+}
 
 // getApi(requestURL, "GOOG");
 
-searchBttnEl.addEventListener("click", function() {
-    getApi(requestURL, inputEl.value);
+
+searchBtnEl.addEventListener("click", function() {
+    getApi(requestURL, txtBoxEl.value);
+    txtBoxEl.value = "";
 })
 
+function init(stockList) {
+    for (var i = 0; i < defaultStocks.length; i++) {
+        result = stockList.find(s => s.T === defaultStocks[i]);
+        stocks.push(result.T + ", close: " + result.c + ", high: " + result.h + ", low: " + result.l + ", transactions: " + result.n);
+        renderStocks(result); 
+    }
+    
+}:
 
-
-// TODO:
-//
+// TODO":
+// most recent buisness day
+// populate initial load with default stocks
+// 
